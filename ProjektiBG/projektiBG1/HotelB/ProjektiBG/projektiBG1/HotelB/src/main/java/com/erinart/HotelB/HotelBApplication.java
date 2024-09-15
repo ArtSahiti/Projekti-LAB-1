dc30681fc69db703e2a68a2e7ff583069b0e3ee8
ArtSahiti <104899505+ArtSahiti@users.noreply.github.com>
2024-09-16 00:35:30 +0200

    Add files via upload
    User,Room,Booking Data Transfer Object

---
+++ ProjektiBG/projektiBG1/HotelB/ProjektiBG/projektiBG1/HotelB/src/main/java/com/erinart/HotelB/HotelBApplication.java
@@ -0,0 +1,13 @@
+package com.erinart.HotelB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBApplication.class, args);
+	}
+
+}
