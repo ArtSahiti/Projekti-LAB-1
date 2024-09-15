4175cfecbb0d31df11cfb79b123518d5081decea
Erinwastaken <99759925+Erinwastaken@users.noreply.github.com>
2024-09-15 23:54:20 +0200

    Add files via upload
    Created 3 entities (Room, Booking, User), Aws S3 service, Custom user details and the main class

---
+++ ProjektiBG/projektiBG1/HotelB/src/main/java/com/erinart/HotelB/service/interfac/IBookingService.java
@@ -0,0 +1,12 @@
+package com.erinart.HotelB.service.interfac;
+
+public interface IBookingService {
+    Response saveBooking(Long roomId, Long userId, Booking bookingRequest);
+
+    Response findBookingByConfirmationCode(String confirmationCode);
+
+    Response getAllBookings();
+
+    Response cancelBooking(Long bookingId);
+
+}
---
+++ ProjektiBG/projektiBG1/HotelB/src/test/java/com/erinart/HotelB/HotelBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erinart.HotelB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
